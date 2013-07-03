fb6d0573c327f492e2933ce5d4edeb7285c58494
Victor Shcherb <victor.shcherb@gmail.com>
2013-07-04 01:14:23 +0200

    Update scripts

--- icons/tools/generatepng.sh
+++ icons/tools/generatepng.sh
@@ -25,17 +25,21 @@ generatePngs() {
   TYPE=$1
   COLOR=$2
   NEG=$3
-  echo "On: ${TYPES[i]}"
+  echo "On: $TYPE"
   for FILE in $SVGFOLDER$1/*.svg; do
       FILENAME=${FILE##/*/}
-      FILENAME=mm_${TYPES[i]}_${FILENAME%.*}
+      if [[ $FILENAME == _* ]]; then
+        continue;
+      fi
+      FILENAME=mm_${TYPE}_${FILENAME%.*}
 
       for (( j = 0 ; j < ${#SIZES[@]} ; j++ )) do
         OUTF=${OUTPUTFOLDER}/${FOLDERS[j]}/
-        if [[ $NEG -ne neg ]]; then
+        if [[ -z $NEG ]]; then
           ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} 'none' 'none' $COLOR ${SIZES[j]} ${OUTF}${FILENAME}
         else
           ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} $COLOR $COLOR '#ffffff'  ${SIZES[j]} ${OUTF}${FILENAME}
+          
         fi
         convert ${OUTF}${FILENAME}.png \( +clone -background "#ffffff" -shadow 8000x2-0+0 \) +swap -background none -layers merge +repage -trim ${OUTF}${FILENAME}_glow.png
       done
@@ -43,21 +47,21 @@ generatePngs() {
     done
 }
 
-generatePngs 'icons8' '#AAAAAA' neg
+generatePngs 'icons8' '#777777' neg
 
-generatePngs 'accommodation' '#0092DA'
-generatePngs 'amenity' '#734A08'
-generatePngs 'barrier' '#666666'
-generatePngs 'education' '#39AC39'
-generatePngs 'food' '#734A08'
-generatePngs 'health' '#DA0092'
-generatePngs 'landuse' '#999999'
-generatePngs 'money' '#000000'
-generatePngs 'place_of_worship' '#000000'
-generatePngs 'poi' '#000000' 
-generatePngs 'power' '#8e7409'  
-generatePngs 'shopping' '#AC39AC' 
-generatePngs 'sport'  '#39AC39'
-generatePngs 'tourist' '#734A08'
-generatePngs 'transport' '#0092DA'
-generatePngs 'water' '#0092DA'
+generatePngs 'accommodation' '#777777' #'#0092DA'
+generatePngs 'amenity' '#777777' #'#734A08'
+generatePngs 'barrier' '#777777' #'#666666'
+generatePngs 'education' '#777777' #'#39AC39'
+generatePngs 'food' '#777777' #'#734A08'
+generatePngs 'health' '#DA0092' #'#DA0092'
+generatePngs 'landuse' '#777777' #'#999999'
+generatePngs 'money' '#777777' #'#000000'
+generatePngs 'place_of_worship' '#777777' #'#000000'
+generatePngs 'poi' '#777777' #'#000000'
+generatePngs 'power' '#777777' #'#8e7409'
+generatePngs 'shopping' '#777777' #'#AC39AC'
+generatePngs 'sport'  '#777777' #'#39AC39'
+generatePngs 'tourist' '#777777' #'#734A08'
+generatePngs 'transport' '#777777' #'#0092DA'
+generatePngs 'water' '#777777' #'#0092DA'
