caf77008658253b23f469a13d0a0255d5a380dfa
Victor Shcherb <victor.shcherb@gmail.com>
2013-07-24 00:00:48 +0200

    Update diving/fishing icons

--- icons/tools/recolour.sh
+++ icons/tools/recolour.sh
@@ -9,16 +9,22 @@ recolour() {
     stroke="$3"
     background="$4"
 sed_script=$(cat << EOF
-    s/fill:#111111/fill:${fill};/g
+    s/fill:#111111;/fill:${fill};/g
     s/fill:#111;/fill:${fill};/g
+    s/fill=\"#111111\"/fill=\"${fill}\"/g
+    s/fill=\"#111\"/fill=\"${fill}\"/g
     s/fill=\"#333333\"/fill=\"${fill}\"/g
-    s/stroke:#eeeeee/stroke:${stroke};/g
+    s/stroke:#eeeeee;/stroke:${stroke};/g
     s/stroke:#eee;/stroke:${stroke};/g
+    s/stroke=\"#eeeeee\"/stroke=\"${stroke}\"/g
     s/stroke=\"#333333\"/stroke=\"${stroke}\"/g
-    s/fill:white/fill:${background};/g
-    s/stroke:white/stroke:${background};/g
-    s/fill:#ffffff/fill:${background};/g
-    s/stroke:#ffffff/stroke:${background};/g
+    s/fill:white/fill:${background}/g
+    s/stroke:white/stroke:${background}/g
+    s/stroke=\"white\"/stroke=\"${background}\"/g
+    s/fill:#ffffff/fill:${background}/g
+    s/fill=\"#ffffff\"/fill=\"${background}\"/g
+    s/stroke:#ffffff/stroke:${background}/g
+    s/stroke=\"#ffffff\"/stroke=\"${background}\"/g
 EOF
 )
     sed "$sed_script" $filename
