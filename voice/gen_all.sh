9b46f5e8a313641a99d4f4424b4434c435de0406
hvdwolf <hvdwolf@gmail.com>
2013-08-01 19:48:49 +0200

    Update gen_all.sh
    Forgot to update the language description itself

--- voice/gen_all.sh
+++ voice/gen_all.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 #./gen_voice.sh de German fest de-m # not supported
+./gen_voice.sh cs Czech google cs
+./gen_voice.sh da Danish google da
 ./gen_voice.sh de German google de
-./gen_voice.sh cs German google cs
-./gen_voice.sh de German google de
-./gen_voice.sh el German google el
+./gen_voice.sh el Greek google el
 ./gen_voice.sh en English fest en-m
 ./gen_voice.sh en English google en
-./gen_voice.sh es German google es
-./gen_voice.sh fi English google fi
-./gen_voice.sh fr English google fr
+./gen_voice.sh es Spanish google es
+./gen_voice.sh fi Finnish google fi
+./gen_voice.sh fr French google fr
 #./gen_voice.sh he English google he
-./gen_voice.sh hi English google hi
-./gen_voice.sh hu English google hu
-./gen_voice.sh it English google it
-./gen_voice.sh ko English google ko
-./gen_voice.sh lv English google lv
+./gen_voice.sh hi Hindi google hi
+./gen_voice.sh hu hungarian google hu
+./gen_voice.sh it Italian google it
+./gen_voice.sh ko Korean google ko
+./gen_voice.sh lv Latvian google lv
 ./gen_voice.sh nl Dutch google nl
 ./gen_voice.sh pl Polish google pl
-./gen_voice.sh pt Polish google pt
-./gen_voice.sh ro Polish google ro
+./gen_voice.sh pt Portuguese google pt
+./gen_voice.sh ro Romanian google ro
 ./gen_voice.sh ru Russian fest ru-m
 ./gen_voice.sh ru Russian google ru
-./gen_voice.sh sk Polish google sk
-./gen_voice.sh sl Polish google sl
-./gen_voice.sh sv Polish google sv
-./gen_voice.sh sw Polish google sw
-./gen_voice.sh zh Polish google zh
+./gen_voice.sh sk Slovak google sk
+./gen_voice.sh sl Slovenian google sl
+./gen_voice.sh sv Swedish google sv
+./gen_voice.sh sw Swahili google sw
+./gen_voice.sh zh Chinese google zh
 
 
 
