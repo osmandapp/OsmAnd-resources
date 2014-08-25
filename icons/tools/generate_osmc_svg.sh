9d46437aea3badc73fd66445af4a895910e542e8
xmd5a2 <lornaderek@mail.ru>
2014-08-25 17:40:08 +0400

    Rename osmc source dir

---
+++ icons/tools/generate_osmc_svg.sh
@@ -0,0 +1,12 @@
+declare -a colors=("black" "white" "green" "red" "yellow" "blue")
+
+## now loop through the above array
+for bg in "${arr[@]}"
+do
+  for bar in "${arr[@]}"
+  do
+      python svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_bar.svg > osmc_${bg}_bg_${bar}_bar.svg
+      python svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_circle.svg > osmc_${bg}_bg_${bar}_circle.svg
+  done
+   # or do whatever with individual element of the array
+done
