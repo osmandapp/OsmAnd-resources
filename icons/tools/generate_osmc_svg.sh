39ece624320295f241bedfe17dff72d2d5912adc
xmd5a2 <lornaderek@mail.ru>
2014-08-25 17:43:03 +0400

    Add script for stacking svg

--- icons/tools/generate_osmc_svg.sh
+++ icons/tools/generate_osmc_svg.sh
@@ -1,3 +1,4 @@
+cd ../svg/osmc_source
 declare -a colors=("black" "white" "green" "red" "yellow" "blue")
 
 ## now loop through the above array
@@ -5,8 +6,8 @@ for bg in "${arr[@]}"
 do
   for bar in "${arr[@]}"
   do
-      python svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_bar.svg > osmc_${bg}_bg_${bar}_bar.svg
-      python svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_circle.svg > osmc_${bg}_bg_${bar}_circle.svg
+      python svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_bar.svg > ../svg/osmc/osmc_${bg}_bg_${bar}_bar.svg
+      python svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_circle.svg > ../svg/osmc/osmc_${bg}_bg_${bar}_circle.svg
   done
    # or do whatever with individual element of the array
 done
