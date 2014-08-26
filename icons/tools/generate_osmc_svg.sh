64d114f987a5395946f76c750202997aaa6206af
xmd5a2 <lornaderek@mail.ru>
2014-08-26 09:35:12 +0400

    Fix generate osmc svg script

--- icons/tools/generate_osmc_svg.sh
+++ icons/tools/generate_osmc_svg.sh
@@ -1,13 +1,14 @@
 cd ../svg/osmc_source
 declare -a colors=("black" "white" "green" "red" "yellow" "blue")
-
 ## now loop through the above array
 for bg in "${colors[@]}"
 do
   for bar in "${colors[@]}"
   do
-     python ../../tools/svg_stack.py --margin=-16 osmc_${bg}_bg.svg osmc_${bar}_bar.svg > ../osmc/h_osmc_${bg}_bg_${bar}_bar.svg
-     python ../../tools/svg_stack.py --margin=-16 osmc_${bg}_bg.svg osmc_${bar}_circle.svg > ../osmc/h_osmc_${bg}_bg_${bar}_circle.svg
+    if [ "$bg" != "$bar" ]; then
+      python ../../tools/svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_bar.svg > ../osmc/osmc_${bg}_bg_${bar}_bar.svg
+      python ../../tools/svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_circle.svg > ../osmc/osmc_${bg}_bg_${bar}_circle.svg
+    fi
   done
-    # or do whatever with individual element of the array
+   # or do whatever with individual element of the array
 done
