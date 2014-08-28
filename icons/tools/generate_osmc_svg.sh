a91163e8e760f5772085149e9f5677cfefb8ef59
xmd5a2 <lornaderek@mail.ru>
2014-08-28 11:17:00 +0400

    osmc: update icons, add icons for "no foreground" case

--- icons/tools/generate_osmc_svg.sh
+++ icons/tools/generate_osmc_svg.sh
@@ -6,9 +6,15 @@ do
   for bar in "${colors[@]}"
   do
     if [ "$bg" != "$bar" ]; then
-      python ../../tools/svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_bar.svg > ../osmc/osmc_${bg}_bg_${bar}_bar.svg
-      python ../../tools/svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_circle.svg > ../osmc/osmc_${bg}_bg_${bar}_circle.svg
+      python ../../tools/svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_bar.svg > ../osmc/h_osmc_${bg}_bg_${bar}_bar.svg
+      python ../../tools/svg_stack.py --margin=-16  osmc_${bg}_bg.svg osmc_${bar}_circle.svg > ../osmc/h_osmc_${bg}_bg_${bar}_circle.svg
     fi
   done
    # or do whatever with individual element of the array
 done
+cp osmc_black_bg.svg ../osmc/h_osmc_black_bg.svg
+cp osmc_blue_bg.svg ../osmc/h_osmc_blue_bg.svg
+cp osmc_green_bg.svg ../osmc/h_osmc_green_bg.svg
+cp osmc_red_bg.svg ../osmc/h_osmc_red_bg.svg
+cp osmc_white_bg.svg ../osmc/h_osmc_white_bg.svg
+cp osmc_yellow_bg.svg ../osmc/h_osmc_yellow_bg.svg
