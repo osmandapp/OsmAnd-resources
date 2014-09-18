437be641c9a6268a6167b087dba3dffd93a72623
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-09-18 12:55:45 +0300

    Fix

--- embed-resources.py
+++ embed-resources.py
@@ -49,12 +49,12 @@ class OsmAndCoreResourcesPacker(object):
         outputFile.write("#if defined(OSMAND_COMPILER_FAMILY_msvc)\n")
         outputFile.write("#   define EMIT_GETTER(varname, accessor)\\\n")
         outputFile.write("        __pragma( comment ( linker, \"/INCLUDE:_\"___STRINGIFY( __get__##varname ) ) )\\\n")
-        outputFile.write("        const void* BUNDLE_API __get__##varname() {\\\n")
+        outputFile.write("        BUNDLE_API const void* __get__##varname() {\\\n")
         outputFile.write("            return reinterpret_cast<const void*>(accessor varname);\\\n")
         outputFile.write("        }\n")
         outputFile.write("#else\n")
         outputFile.write("#   define EMIT_GETTER(varname, accessor)\\\n")
-        outputFile.write("        const void* BUNDLE_API __get__##varname() {\\\n")
+        outputFile.write("        BUNDLE_API const void* __get__##varname() {\\\n")
         outputFile.write("            return reinterpret_cast<const void*>(accessor varname);\\\n")
         outputFile.write("        }\n")
         outputFile.write("#endif // defined(OSMAND_COMPILER_FAMILY_msvc)\n")
@@ -86,7 +86,7 @@ class OsmAndCoreResourcesPacker(object):
             # Write content
             for (byteIdx, byteValue) in enumerate(packedContent):
                 if byteIdx % 16 == 0:
-                    outputFile.write("\n\t\t")
+                    outputFile.write("\n\t")
                 outputFile.write("0x%02x, " % (byteValue))
             outputFile.write("\n")
             outputFile.write("};\n")
@@ -102,7 +102,7 @@ class OsmAndCoreResourcesPacker(object):
         outputFile.write("const uint32_t __CoreResourcesEmbeddedBundle__ResourcesCount = %d;\n" % (len(resources)))
         outputFile.write("EMIT_GETTER(__CoreResourcesEmbeddedBundle__ResourcesCount, &)\n")
         outputFile.write("\n")
-        outputFile.write("void BUNDLE_API __CoreResourcesEmbeddedBundle__FakeReferences() {\\\n")
+        outputFile.write("BUNDLE_API void __CoreResourcesEmbeddedBundle__FakeReferences() {\\\n")
         for (idx, resource) in enumerate(resources):
             outputFile.write("    GETTER_NAME(__CoreResourcesEmbeddedBundle__ResourceName_%d)();\n" % (idx))
             outputFile.write("    GETTER_NAME(__CoreResourcesEmbeddedBundle__ResourceData_%d)();\n" % (idx))
