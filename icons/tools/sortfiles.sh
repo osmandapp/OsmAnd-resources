937f95419e40a372c2be792b83672b45036f2f6a
xmd5a <lornaderek@mail.ru>
2014-11-29 17:39:01 +0300

    Add natural=geyser to rendering

--- icons/tools/sortfiles.sh
+++ icons/tools/sortfiles.sh
@@ -54,6 +54,7 @@ mycp natural volcano_big functional-icons_volcano_big.png
 mycp natural cave_entrance poi_cave.png
 mycp natural spring water_spring.png  #scale 0.375
 mycp natural hot_spring water_hot_spring.png  #scale 0.375
+mycp natural geyser water_geyser.png  #scale 0.375
 mycp natural tree functional-icons_tree.png  #scale 0.25
 mycp natural tree_broadleaved functional-icons_tree_broadleaved.png
 mycp natural tree_needleleaved functional-icons_tree_needleleaved.png
