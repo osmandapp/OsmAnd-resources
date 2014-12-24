a2060c038d63877b6632104937b1ec7cd045ab6a
xmd5a <lornaderek@mail.ru>
2014-12-24 21:37:11 +0300

    Update generatepng.sh. Add x4 option.

--- icons/tools/generatepng.sh
+++ icons/tools/generatepng.sh
@@ -7,7 +7,13 @@ popd  > /dev/null
 BASEFOLDER=`dirname $BASEFOLDER`
 
 FOLDERS=(big-xxhdpi big-xhdpi big-hdpi big-mdpi xxhdpi xhdpi hdpi mdpi)
-FOLDERS2=("${FOLDERS[@]}")
+FOLDERS_NOMX=(xxhdpi xhdpi hdpi mdpi) # no icons used in osmand interface (search, poi overlay)
+SIZES=(96 64 48 32 48 32 24 16)
+SIZESx2=(96 64 48 32 96 64 48 32)
+SIZESx4=(96 64 48 32 192 128 96 64)
+
+FOLDERS_ORIG=("${FOLDERS[@]}")
+SIZES_ORIG=("${SIZES[@]}")
 
 SVGFOLDER=${BASEFOLDER}/svg/
 OUTPUTFOLDER=${BASEFOLDER}/png/
@@ -40,23 +46,33 @@ generatePngs() {
   COLOR=$2
   COLOR2=$3
   NEG=$4
-  LARGE_ICONS=$5
-  NO_MX=$6
-  echo "On: $TYPE : $LARGE_ICONS $NO_MX"
-  if [ "$LARGE_ICONS" = 'large' ]
+  X2_ICONS=$5
+  X4_ICONS=$6
+  NO_MX=$7
+  echo "On : $TYPE : $X2_ICONS $X4_ICONS $NO_MX"
+  if [ "$X4_ICONS" = 'x4' ]
     then
-     SIZES=(96 64 48 32 96 64 48 32)
-     echo "     ${SIZES[@]}  (5-8 with nomx)"
+     SIZES=("${SIZESx4[@]}")
+     echo " x4  ${SIZES[@]}"
     else
-     SIZES=(96 64 48 32 48 32 24 16)
-     echo "     ${SIZES[@]}  (5-8 with nomx)"
+     SIZES=("${SIZES_ORIG[@]}")
+     echo "     ${SIZES[@]} "
   fi
+  if [ "$X2_ICONS" = 'x2' ]
+    then
+     SIZES=("${SIZESx2[@]}")
+     echo " x2  ${SIZES[@]}"
+  fi
+
   if [ "$NO_MX" = 'nomx' ]
     then
-	FOLDERS=(xxhdpi xhdpi hdpi mdpi)
+	let "SIZES_HALF=${#SIZES[@]} / 2"
+	SIZES=("${SIZES[@]:$SIZES_HALF}")
+	echo "nomx:"${SIZES[@]}
+	FOLDERS=("${FOLDERS_NOMX[@]}")
 	echo "     ${FOLDERS[@]}"
     else
-	FOLDERS=("${FOLDERS2[@]}")
+	FOLDERS=("${FOLDERS_ORIG[@]}")
  	echo "     ${FOLDERS[@]}"
  fi
   for FILE in $SVGFOLDER$1/*.svg; do
@@ -65,52 +81,52 @@ generatePngs() {
         continue;
       fi
       FILENAME=${TYPE}_${FILENAME%.*}
-
-      for (( j = 0 ; j < $( if [ "$NO_MX" = 'nomx' ]; then let "c=${#SIZES[@]} / 2"; echo $c ; else echo ${#SIZES[@]}; fi) ; j++ )) do
+      for (( j = 0 ; j < ${#SIZES[@]}; j++ )) do
         OUTF=${OUTPUTFOLDER}${FOLDERS[j]}/
         if [[ -z $NEG ]]; then
-          ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} 'none' 'none' $COLOR ${SIZES[j]} ${OUTF}${FILENAME}
-          ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} 'none' 'none' $COLOR2 ${SIZES[j]} ${OUTF}${FILENAME}_2
+          ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} 'none' 'none' $COLOR ${SIZES[j]} ${OUTF}${FILENAME} > /dev/null 2>&1
+          ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} 'none' 'none' $COLOR2 ${SIZES[j]} ${OUTF}${FILENAME}_2 > /dev/null 2>&1
         else
-          ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} $COLOR $COLOR '#ffffff'  ${SIZES[j]} ${OUTF}${FILENAME}
-          ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} ${COLOR2} ${COLOR2} '#ffffff'  ${SIZES[j]} ${OUTF}${FILENAME}_2
+          ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} $COLOR $COLOR '#ffffff'  ${SIZES[j]} ${OUTF}${FILENAME} > /dev/null 2>&1
+          ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} ${COLOR2} ${COLOR2} '#ffffff'  ${SIZES[j]} ${OUTF}${FILENAME}_2 > /dev/null 2>&1
         fi
         # convert ${OUTF}${FILENAME}.png \( +clone -background "#ffffff" -shadow 8000x2-0+0 \) +swap -background none -layers merge +repage -trim ${OUTF}${FILENAME}_glow.png
       done
     done
 }
 
-    generateElements 'shaders'
-    generateElements 'osmc'
-    generatePngs 'seamark' '#777777' '#777777' '' large nomx
+      generateElements 'shaders'
+      generateElements 'osmc'
+      generatePngs 'seamark' '#777777' '#777777' '' x2 '' nomx
+      generatePngs 'seamark_overlays' '#777777' '#777777' '' '' x4 nomx
 
-    generatePngs 'functional-icons' '#777777' '#777777'
-    generatePngs 'water' '#0092DA' '#777777'
+      generatePngs 'functional-icons' '#777777' '#777777'
+      generatePngs 'water' '#0092DA' '#777777'
 
-    generatePngs 'emergency' '#DA0092' '#777777'
-    generatePngs 'health' '#DA0092' '#777777'
+      generatePngs 'emergency' '#DA0092' '#777777'
+      generatePngs 'health' '#DA0092' '#777777'
 
-    generatePngs 'transport' '#0092DA' '#777777'
+      generatePngs 'transport' '#0092DA' '#777777'
 
-    generatePngs 'barrier' '#444444' '#777777'
+      generatePngs 'barrier' '#444444' '#777777'
 
-    generatePngs 'accommodation' '#0092DA' '#777777'
+      generatePngs 'accommodation' '#0092DA' '#777777'
 
-    generatePngs 'tourist' '#734A08' '#777777'
+      generatePngs 'tourist' '#734A08' '#777777'
 
-    generatePngs 'sport'  '#39AC39' '#777777'
+      generatePngs 'sport'  '#39AC39' '#777777'
 
-    generatePngs 'amenity' '#777777' '#734A08'
-    generatePngs 'place_of_worship' '#333333' '#777777'
-    generatePngs 'money' '#777777' '#777777'
-    generatePngs 'education' '#777777' '#39AC39'
-    generatePngs 'poi' '#777777' '#777777' 
-    generatePngs 'power' '#777777' '#8e7409'
+      generatePngs 'amenity' '#777777' '#734A08'
+      generatePngs 'place_of_worship' '#333333' '#777777'
+      generatePngs 'money' '#777777' '#777777'
+      generatePngs 'education' '#777777' '#39AC39'
+      generatePngs 'poi' '#777777' '#777777' 
+      generatePngs 'power' '#777777' '#8e7409'
 
-    generatePngs 'food' '#777777' '#734A08'
+      generatePngs 'food' '#777777' '#734A08'
 
-    generatePngs 'shopping' '#777777' '#AC39AC'
+      generatePngs 'shopping' '#777777' '#AC39AC'
 
-    generatePngs 'landuse' '#777777' '#999999'
+      generatePngs 'landuse' '#777777' '#999999'
 
-    generatePngs 'icons8' '#777777' '#777777' neg
+      generatePngs 'icons8' '#777777' '#777777' neg
