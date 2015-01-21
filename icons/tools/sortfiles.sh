10a6378d97f6fe1937a3108f18544b1957146b13
xmd5a <lornaderek@mail.ru>
2015-01-21 23:34:30 +0300

    Tweak street light

--- icons/tools/sortfiles.sh
+++ icons/tools/sortfiles.sh
@@ -127,6 +127,7 @@ mycp highway mini_roundabout transport_miniroundabout_anticlockwise.png
 mycp highway noexit functional-icons_noexit.png
 mycp highway ford transport_ford.png
 mycp highway crossing transport_zebra_crossing2.png
+mycp highway crossing_bg functional-icons_highway_crossing_bg.png
 mycp highway crossing_unmarked functional-icons_highway_crossing_unmarked.png
 mycp highway crossing_uncontrolled functional-icons_highway_crossing_uncontrolled.png
 mycp highway crossing_traffic_signals functional-icons_highway_crossing_traffic_signals.png
