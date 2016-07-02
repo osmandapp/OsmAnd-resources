fcba5cb4912bd95123222a21370d22ae0ddf4454
Victor Shcherb <victor.shcherb@gmail.com>
2016-07-02 21:29:11 +0200

    Add pbf proto files

--- protos/proto_generate.sh
+++ protos/proto_generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 protobuf/src/protoc --java_out=${DIR}/../../android/OsmAnd-java/src --proto_path=${DIR} ${DIR}/OBF.proto
+protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/osmformat.proto
+protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/fileformat.proto
