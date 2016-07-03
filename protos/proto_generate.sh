d8dd1e573bae2565bbc12e966e652c7d895918c4
Victor Shcherb <victor.shcherb@gmail.com>
2016-07-03 17:42:57 +0200

    Add support for C++ library proto

--- protos/proto_generate.sh
+++ protos/proto_generate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 protobuf/src/protoc --java_out=${DIR}/../../android/OsmAnd-java/src --proto_path=${DIR} ${DIR}/OBF.proto
+# protobuf/src/protoc --cpp_out=${DIR}/../../core/protos/ --proto_path=${DIR} ${DIR}/OBF.proto
+# TODO core-legacy and other formats
 protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/osmformat.proto
 protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/fileformat.proto
