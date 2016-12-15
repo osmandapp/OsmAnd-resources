7f5254aa6bbb133385da9990c9fd1f8355d550aa
xmd5a <lornaderek@mail.ru>
2016-12-15 17:06:37 +0300

    Add landuse=animal_keeping

--- icons/tools/sortfiles.sh
+++ icons/tools/sortfiles.sh
@@ -888,6 +888,7 @@ mycpname barbecue amenity_barbecue.png
 mycpname public_bookcase amenity_public_bookcase.png
 mycpname internet_cafe amenity_internet_cafe.png
 mycpname feeding_place amenity_feeding_place.png
+mycpname animal_keeping amenity_animal_keeping.png
 
 mygroup craft craft_tailor.png
 mycpname craft_beekeeper craft_beekeeper.png
