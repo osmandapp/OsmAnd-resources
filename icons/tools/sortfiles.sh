f882ed1ab036c8a5da149178dd6c627d954189ab
xmd5a <lornaderek@mail.ru>
2018-03-21 11:28:07 +0300

    Add delivery icons

--- icons/tools/sortfiles.sh
+++ icons/tools/sortfiles.sh
@@ -2318,6 +2318,8 @@ mycpname motorcycle_repair_no additional_motorcycle_repair_no.png
 mycpname motorcycle_repair_yes additional_motorcycle_repair_yes.png
 mycpname motorcycle_sales_no additional_motorcycle_sales_no.png
 mycpname motorcycle_sales_yes additional_motorcycle_sales_yes.png
+mycpname delivery_yes additional_delivery_yes.png
+mycpname delivery_no additional_delivery_no.png
 
 #Additional category
 mycpname payment_type additional_category_payment_type.png
