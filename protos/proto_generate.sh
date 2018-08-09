2263523393197fd1a7f3b09437847924f44280b2
Victor Shcherb <victor.shcherb@gmail.com>
2018-08-09 17:57:33 +0200

    Update protobuf

--- protos/proto_generate.sh
+++ protos/proto_generate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-protobuf/src/protoc --java_out=${DIR}/../../android/OsmAnd-java/src --proto_path=${DIR} ${DIR}/OBF.proto
+protobuf/src/protoc --java_out=${DIR}/../../android/OsmAnd-java/src/main/java --proto_path=${DIR} ${DIR}/OBF.proto
 # protobuf/src/protoc --cpp_out=${DIR}/../../core/protos/ --proto_path=${DIR} ${DIR}/OBF.proto
-# TODO core-legacy and other formats
-protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/osmformat.proto
-protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/fileformat.proto
+# protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/osmformat.proto
+# protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/fileformat.proto
