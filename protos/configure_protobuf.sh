31a118fefb8cbacd1ff1be52919d555d34f3591d
crimean <alexey.kulish@osmand.net>
2018-09-20 20:35:08 +0300

    Fix protobuf script. Add namespace to osmand index.

--- protos/configure_protobuf.sh
+++ protos/configure_protobuf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ ! -d protobuf ]; then
    git clone https://github.com/google/protobuf.git
-   cd protobuf && git checkout v2.5.0 && ./configure && cd - 
+   cd protobuf && git checkout v2.5.0
+   patch --directory=.. -p0 --input=autogen-protobuf-2.5.patch
+   ./autogen.sh && ./configure && cd - 
 fi 
 cd protobuf
 git checkout .
@@ -12,5 +14,4 @@ rm -rf protobuf-java/*
 cp -rf protobuf/java/src/main/java/* protobuf-java/
 patch --directory=. -p0 --input=protobuf-2.5.patch
 protobuf/src/protoc --java_out=protobuf-java/ --proto_path=protobuf/src/google/protobuf/ protobuf/src/google/protobuf/descriptor.proto 
-cp -rf protobuf-java/* $(pwd)/../../android/OsmAnd-java/src/
- 
+cp -rf protobuf-java/* $(pwd)/../../android/OsmAnd-java/src/main/java
