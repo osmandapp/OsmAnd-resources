4f1f060072a33f178ca43070f5968e764b27ff8d
xmd5a <lornaderek@mail.ru>
2018-12-27 11:01:51 +0300

    Add healthcare=laboratory (fix https://github.com/osmandapp/Osmand/issues/6379)

--- icons/tools/sortfiles.sh
+++ icons/tools/sortfiles.sh
@@ -228,6 +228,7 @@ mycp amenity veterinary health_veterinary.png
 mycpname social_facility health_nursing_home.png
 mycpname sanatorium health_sanatorium.png
 mycpname health_amenity_type_first_aid_kit health_health_amenity_type_first_aid_kit.png
+mycpname laboratory health_laboratory.png
 
 ## Overlays
 mycp fixme_tag yes overlays_fixme.png
