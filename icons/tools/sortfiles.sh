bdc0e4eb754472e650665e5a32dd796ae4d10d00
xmd5a <lornaderek@mail.ru>
2019-09-18 10:13:36 +0300

    Add shop=health_food (fix https://github.com/osmandapp/Osmand/issues/7560)

--- icons/tools/sortfiles.sh
+++ icons/tools/sortfiles.sh
@@ -1898,6 +1898,7 @@ mycpname outpost shopping_outpost.png
 mycpname appliance shopping_appliance.png
 mycpname shoe_repair shopping_shoe_repair.png
 mycpname shop_wholesale shopping_wholesale.png
+mycpname health_food shopping_health_food.png
 
 ## Office
 mycpname estate_agent office_estate_agent.png
