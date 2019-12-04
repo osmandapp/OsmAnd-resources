2f51b8dce541ec839db91a7f34b2b5a756d77538
max-klaus <max.klaus@osmand.net>
2019-12-04 17:19:14 +0300

    Added poi phrases for ja

--- poi/copy_phrases.sh
+++ poi/copy_phrases.sh
@@ -21,6 +21,7 @@ cp -f ../../android/OsmAnd/res/values-cs/phrases.xml phrases/cs/
 cp -f ../../android/OsmAnd/res/values-gl/phrases.xml phrases/gl/
 cp -f ../../android/OsmAnd/res/values-tr/phrases.xml phrases/tr/
 cp -f ../../android/OsmAnd/res/values-uk/phrases.xml phrases/uk/
+cp -f ../../android/OsmAnd/res/values-ja/phrases.xml phrases/ja/
 
 cp -f ../../android/OsmAnd/res/values-b+hsb/phrases.xml phrases/hsb/
 #cp -f ../../android/OsmAnd/res/values-ku/phrases.xml phrases/ku/
