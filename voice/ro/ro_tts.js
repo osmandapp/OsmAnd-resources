3b6cf52ef45bb194580896db4f8ad1a9e33e373e
Buraddo <vladsing@gmail.com>
2019-12-19 16:16:14 +0200

    Added logic for number 1 and deleted spaces

--- voice/ro/ro_tts.js
+++ voice/ro/ro_tts.js
@@ -135,13 +135,14 @@ function populateDictionary(tts) {
 	dictionary["1_minute"] = tts ? "un minut" : "1_minute.ogg";
 	dictionary["minutes"] = tts ? "minute" : "minutes.ogg";
     
-    // FEMININE FORM NUMBERS
+    // GENDERED NUMBERS
+    dictionary["1_masc"] = tts ? "un" : "1_masc.ogg";
     dictionary["1_fem"] = tts ? "o" : "1_fem.ogg";
     dictionary["2_fem"] = tts ? "două" : "2_fem.ogg";
     dictionary["12_fem"] = tts ? "douăsprezece" : "12_fem.ogg";
     dictionary["x1_fem"] = tts ? "una" : "x1_fem.ogg";
     
-    //PREPOSITIONS
+    // PREPOSITIONS
     dictionary["of"] = tts ? "de" : "of.ogg";
     dictionary["and"] = tts ? "și" : "and.ogg";
 }
@@ -180,47 +181,47 @@ function distance(dist) {
 			break;
 		case "mi-f":
 			if (dist < 160) {
-				return (tts ? number_form(Math.round(2*dist/100.0/0.3048)*50) : ogg_dist(Math.round(2*dist/100.0/0.3048)*50)) + " " + dictionary["feet"];
+				return (tts ? number_form(Math.round(2*dist/100.0/0.3048)*50, "n") : ogg_dist(Math.round(2*dist/100.0/0.3048)*50)) + " " + dictionary["feet"];
 			} else if (dist < 241) {
 				return dictionary["1_tenth_of_a_mile"];
 			} else if (dist < 1529) {
-				return (tts ? number_form(Math.round(dist/161.0)) : ogg_dist(dist/161.0)) + " " + dictionary["tenths_of_a_mile"];
+				return (tts ? number_form(Math.round(dist/161.0), "f") : ogg_dist(dist/161.0)) + " " + dictionary["tenths_of_a_mile"];
 			} else if (dist < 2414) {
 				return dictionary["around_1_mile"];
 			} else if (dist < 16093) {
-				return dictionary["around"] + " " + (tts ? number_form(Math.round(dist/1609.3)) : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
+				return dictionary["around"] + " " + (tts ? number_form(Math.round(dist/1609.3), "f") : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
 			} else {
-				return (tts ? number_form(Math.round(dist/1609.3)) : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
+				return (tts ? number_form(Math.round(dist/1609.3), "f") : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
 			}
 			break;
 		case "mi-m":
 			if (dist < 17) {
-				return (tts ? number_form(Math.round(dist)) : ogg_dist(dist)) + " " + dictionary["meters"];
+				return (tts ? number_form(Math.round(dist), "m") : ogg_dist(dist)) + " " + dictionary["meters"];
 			} else if (dist < 100) {
-				return (tts ? number_form(Math.round(dist/10.0)*10) : ogg_dist(Math.round(dist/10.0)*10)) + " " + dictionary["meters"];
+				return (tts ? number_form(Math.round(dist/10.0)*10, "m") : ogg_dist(Math.round(dist/10.0)*10)) + " " + dictionary["meters"];
 			} else if (dist < 1300) {
-				return (tts ? number_form(Math.round(2*dist/100.0)*50) : ogg_dist(Math.round(2*dist/100.0)*50)) + " " + dictionary["meters"];
+				return (tts ? number_form(Math.round(2*dist/100.0)*50, "m") : ogg_dist(Math.round(2*dist/100.0)*50)) + " " + dictionary["meters"];
 			} else if (dist < 2414) {
 				return dictionary["around_1_mile"];
 			} else if (dist < 16093) {
-				return dictionary["around"] + " " + (tts ? number_form(Math.round(dist/1609.3)) : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
+				return dictionary["around"] + " " + (tts ? number_form(Math.round(dist/1609.3), "f") : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
 			} else {
-				return (tts ? number_form(Math.round(dist/1609.3)) : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
+				return (tts ? number_form(Math.round(dist/1609.3), "f") : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
 			}
 			break;
 		case "mi-y":
 			if (dist < 17) {
-				return (tts ? number_form(Math.round(dist/0.9144)) : ogg_dist(dist/0.9144)) + " " + dictionary["yards"];
+				return (tts ? number_form(Math.round(dist/0.9144), "m") : ogg_dist(dist/0.9144)) + " " + dictionary["yards"];
 			} else if (dist < 100) {
-				return (tts ? number_form(Math.round(dist/10.0/0.9144)*10) : ogg_dist((dist/10.0/0.9144)*10)) + " " + dictionary["yards"];
+				return (tts ? number_form(Math.round(dist/10.0/0.9144)*10, "m") : ogg_dist((dist/10.0/0.9144)*10)) + " " + dictionary["yards"];
 			} else if (dist < 1300) {
-				return (tts ? number_form(Math.round(2*dist/100.0/0.9144)*50) : ogg_dist((2*dist/10.0/0.9144)*10)) + " " + dictionary["yards"]; 
+				return (tts ? number_form(Math.round(2*dist/100.0/0.9144)*50, "m") : ogg_dist((2*dist/10.0/0.9144)*10)) + " " + dictionary["yards"]; 
 			} else if (dist < 2414) {
 				return dictionary["around_1_mile"];
 			} else if (dist < 16093) {
-				return dictionary["around"] + " " + (tts ? number_form(Math.round(dist/1609.3)) : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
+				return dictionary["around"] + " " + (tts ? number_form(Math.round(dist/1609.3), "f") : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
 			} else {
-				return (tts ? number_form(Math.round(dist/1609.3)) : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
+				return (tts ? number_form(Math.round(dist/1609.3), "f") : ogg_dist(dist/1609.3)) + " " + dictionary["miles"];
 			}
 			break;
 	}
@@ -450,7 +451,9 @@ function number_form(number, nounGender) {
             numeral += number.toString();
         }
     } else if (nounGender == "n") {
-        if (number == 2) {
+        if (number == 1) {
+            numeral += dictionary["1_masc"];
+        }else if (number == 2) {
             numeral += dictionary["2_fem"];
         } else if (number == 12) {
             numeral += dictionary["12_fem"];
@@ -462,11 +465,15 @@ function number_form(number, nounGender) {
             numeral += number.toString();
         }
     } else { //masculine or default
-        numeral += number.toString();
+        if (number == 1) {
+            numeral += dictionary["1_masc"];
+        } else {
+            numeral += number.toString();
+        }
     } 
     
     //"de" preposition adding
-    if ((number % 100 == 0) || (number % 100 > 19)) {
+    if (((number % 100 == 0) && (number != 0)) || (number % 100 > 19)) {
         numeral += " " + dictionary["of"];
     }
     
