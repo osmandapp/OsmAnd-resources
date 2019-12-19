b6be5f106757381206a8db8a5de158c27c03fc37
Buraddo <vladsing@gmail.com>
2019-12-19 18:49:47 +0200

    reverted edit on sh file

--- voice/gen_all.sh
+++ voice/gen_all.sh
@@ -35,7 +35,7 @@ fi
 
 # ./gen_voice.sh hr Croatian $ENGINE hr 1.1 yes          sounds really bad
 # ./gen_voice.sh lv Latvian $ENGINE lv 1.1 yes          sounds really bad
-# ./gen_voice.sh ro Romanian $ENGINE ro 1.1 yes  #        sounds really bad
+# ./gen_voice.sh ro Romanian $ENGINE ro 1.1 yes          sounds really bad
 # ./gen_voice.sh sw Swahili $ENGINE sw 1.1 yes          sounds really bad
 
 #./gen_voice.sh be Belarussian $ENGINE be 1.1 yes    not supported by Google API
