98e1b0c1d44e28fd2404a9090c23ea26156622c7
xmd5a <lornaderek@mail.ru>
2021-11-08 17:23:11 +0300

    Add shop=tool_hire (fix https://github.com/osmandapp/OsmAnd-Issues/issues/902)

--- icons/tools/sortfiles.sh
+++ icons/tools/sortfiles.sh
@@ -1963,6 +1963,7 @@ icon_alias nuts shopping_nuts
 icon_alias pest_control shopping_pest_control
 icon_alias groundskeeping shopping_groundskeeping
 icon_alias country_store shopping_country_store
+icon_alias tool_hire shopping_tool_hire
 
 ## Office
 icon_alias estate_agent office_estate_agent
