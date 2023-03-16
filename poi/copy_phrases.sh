909b423cb79597480e18c86a626bad6d5f23cda6
PaulStets <stetsenko.pavlo@gmail.com>
2023-03-16 11:57:32 +0200

    Fix copy_phrases for ios

--- poi/copy_phrases.sh
+++ poi/copy_phrases.sh
@@ -1,90 +1,90 @@
-cp -f ../../android/OsmAnd/res/values/phrases.xml phrases/en/
+rsync -a ../../android/OsmAnd/res/values/phrases.xml phrases/en/
 
 
-cp -f ../../android/OsmAnd/res/values-an/phrases.xml phrases/an/
-cp -f ../../android/OsmAnd/res/values-ar/phrases.xml phrases/ar/
-cp -f ../../android/OsmAnd/res/values-ars/phrases.xml phrases/ars/
-cp -f ../../android/OsmAnd/res/values-ast/phrases.xml phrases/ast/
-cp -f ../../android/OsmAnd/res/values-az/phrases.xml phrases/az/
+rsync -a ../../android/OsmAnd/res/values-an/phrases.xml phrases/an/
+rsync -a ../../android/OsmAnd/res/values-ar/phrases.xml phrases/ar/
+rsync -a ../../android/OsmAnd/res/values-ars/phrases.xml phrases/ars/
+rsync -a ../../android/OsmAnd/res/values-ast/phrases.xml phrases/ast/
+rsync -a ../../android/OsmAnd/res/values-az/phrases.xml phrases/az/
 
-cp -f ../../android/OsmAnd/res/values-b+hsb/phrases.xml phrases/hsb/
-cp -f ../../android/OsmAnd/res/values-b+kab/phrases.xml phrases/kab/
-cp -f ../../android/OsmAnd/res/values-be/phrases.xml phrases/be/
-cp -f ../../android/OsmAnd/res/values-bg/phrases.xml phrases/bg/
-cp -f ../../android/OsmAnd/res/values-bn/phrases.xml phrases/bn/
-cp -f ../../android/OsmAnd/res/values-br/phrases.xml phrases/br/
+rsync -a ../../android/OsmAnd/res/values-b+hsb/phrases.xml phrases/hsb/
+rsync -a ../../android/OsmAnd/res/values-b+kab/phrases.xml phrases/kab/
+rsync -a ../../android/OsmAnd/res/values-be/phrases.xml phrases/be/
+rsync -a ../../android/OsmAnd/res/values-bg/phrases.xml phrases/bg/
+rsync -a ../../android/OsmAnd/res/values-bn/phrases.xml phrases/bn/
+rsync -a ../../android/OsmAnd/res/values-br/phrases.xml phrases/br/
 
-cp -f ../../android/OsmAnd/res/values-ca/phrases.xml phrases/ca/
-cp -f ../../android/OsmAnd/res/values-cs/phrases.xml phrases/cs/
-cp -f ../../android/OsmAnd/res/values-cy/phrases.xml phrases/cy/
+rsync -a ../../android/OsmAnd/res/values-ca/phrases.xml phrases/ca/
+rsync -a ../../android/OsmAnd/res/values-cs/phrases.xml phrases/cs/
+rsync -a ../../android/OsmAnd/res/values-cy/phrases.xml phrases/cy/
 
-cp -f ../../android/OsmAnd/res/values-da/phrases.xml phrases/da/
-cp -f ../../android/OsmAnd/res/values-de/phrases.xml phrases/de/
+rsync -a ../../android/OsmAnd/res/values-da/phrases.xml phrases/da/
+rsync -a ../../android/OsmAnd/res/values-de/phrases.xml phrases/de/
 
-cp -f ../../android/OsmAnd/res/values-el/phrases.xml phrases/el/
-cp -f ../../android/OsmAnd/res/values-en-rGB/phrases.xml phrases/en-GB/
-cp -f ../../android/OsmAnd/res/values-eo/phrases.xml phrases/eo/
-cp -f ../../android/OsmAnd/res/values-es/phrases.xml phrases/es/
-cp -f ../../android/OsmAnd/res/values-es-rAR/phrases.xml phrases/es-AR/
-cp -f ../../android/OsmAnd/res/values-es-rUS/phrases.xml phrases/es-US/
-cp -f ../../android/OsmAnd/res/values-et/phrases.xml phrases/et/
-cp -f ../../android/OsmAnd/res/values-eu/phrases.xml phrases/eu/
+rsync -a ../../android/OsmAnd/res/values-el/phrases.xml phrases/el/
+rsync -a ../../android/OsmAnd/res/values-en-rGB/phrases.xml phrases/en-GB/
+rsync -a ../../android/OsmAnd/res/values-eo/phrases.xml phrases/eo/
+rsync -a ../../android/OsmAnd/res/values-es/phrases.xml phrases/es/
+rsync -a ../../android/OsmAnd/res/values-es-rAR/phrases.xml phrases/es-AR/
+rsync -a ../../android/OsmAnd/res/values-es-rUS/phrases.xml phrases/es-US/
+rsync -a ../../android/OsmAnd/res/values-et/phrases.xml phrases/et/
+rsync -a ../../android/OsmAnd/res/values-eu/phrases.xml phrases/eu/
 
-cp -f ../../android/OsmAnd/res/values-fa/phrases.xml phrases/fa/
-cp -f ../../android/OsmAnd/res/values-fi/phrases.xml phrases/fi/
-cp -f ../../android/OsmAnd/res/values-fr/phrases.xml phrases/fr/
+rsync -a ../../android/OsmAnd/res/values-fa/phrases.xml phrases/fa/
+rsync -a ../../android/OsmAnd/res/values-fi/phrases.xml phrases/fi/
+rsync -a ../../android/OsmAnd/res/values-fr/phrases.xml phrases/fr/
 
-cp -f ../../android/OsmAnd/res/values-gl/phrases.xml phrases/gl/
+rsync -a ../../android/OsmAnd/res/values-gl/phrases.xml phrases/gl/
 
-cp -f ../../android/OsmAnd/res/values-iw/phrases.xml phrases/he/
-cp -f ../../android/OsmAnd/res/values-hr/phrases.xml phrases/hr/
-cp -f ../../android/OsmAnd/res/values-hu/phrases.xml phrases/hu/
+rsync -a ../../android/OsmAnd/res/values-iw/phrases.xml phrases/he/
+rsync -a ../../android/OsmAnd/res/values-hr/phrases.xml phrases/hr/
+rsync -a ../../android/OsmAnd/res/values-hu/phrases.xml phrases/hu/
 
-cp -f ../../android/OsmAnd/res/values-in/phrases.xml phrases/id/
-cp -f ../../android/OsmAnd/res/values-is/phrases.xml phrases/is/
-cp -f ../../android/OsmAnd/res/values-it/phrases.xml phrases/it/
+rsync -a ../../android/OsmAnd/res/values-in/phrases.xml phrases/id/
+rsync -a ../../android/OsmAnd/res/values-is/phrases.xml phrases/is/
+rsync -a ../../android/OsmAnd/res/values-it/phrases.xml phrases/it/
 
-cp -f ../../android/OsmAnd/res/values-ja/phrases.xml phrases/ja/
+rsync -a ../../android/OsmAnd/res/values-ja/phrases.xml phrases/ja/
 
-cp -f ../../android/OsmAnd/res/values-ka/phrases.xml phrases/ka/
-cp -f ../../android/OsmAnd/res/values-kn/phrases.xml phrases/kn/
-cp -f ../../android/OsmAnd/res/values-ko/phrases.xml phrases/ko/
-## cp -f ../../android/OsmAnd/res/values-ku/phrases.xml phrases/ku/
+rsync -a ../../android/OsmAnd/res/values-ka/phrases.xml phrases/ka/
+rsync -a ../../android/OsmAnd/res/values-kn/phrases.xml phrases/kn/
+rsync -a ../../android/OsmAnd/res/values-ko/phrases.xml phrases/ko/
+## rsync -a ../../android/OsmAnd/res/values-ku/phrases.xml phrases/ku/
 
-cp -f ../../android/OsmAnd/res/values-lt/phrases.xml phrases/lt/
-cp -f ../../android/OsmAnd/res/values-lv/phrases.xml phrases/lv/
+rsync -a ../../android/OsmAnd/res/values-lt/phrases.xml phrases/lt/
+rsync -a ../../android/OsmAnd/res/values-lv/phrases.xml phrases/lv/
 
-cp -f ../../android/OsmAnd/res/values-mk/phrases.xml phrases/mk/
-cp -f ../../android/OsmAnd/res/values-ml/phrases.xml phrases/ml/
-cp -f ../../android/OsmAnd/res/values-mr/phrases.xml phrases/mr/
-cp -f ../../android/OsmAnd/res/values-my/phrases.xml phrases/my/
+rsync -a ../../android/OsmAnd/res/values-mk/phrases.xml phrases/mk/
+rsync -a ../../android/OsmAnd/res/values-ml/phrases.xml phrases/ml/
+rsync -a ../../android/OsmAnd/res/values-mr/phrases.xml phrases/mr/
+rsync -a ../../android/OsmAnd/res/values-my/phrases.xml phrases/my/
 
-cp -f ../../android/OsmAnd/res/values-nb/phrases.xml phrases/nb/
-cp -f ../../android/OsmAnd/res/values-nl/phrases.xml phrases/nl/
+rsync -a ../../android/OsmAnd/res/values-nb/phrases.xml phrases/nb/
+rsync -a ../../android/OsmAnd/res/values-nl/phrases.xml phrases/nl/
 
-cp -f ../../android/OsmAnd/res/values-oc/phrases.xml phrases/oc/
+rsync -a ../../android/OsmAnd/res/values-oc/phrases.xml phrases/oc/
 
-cp -f ../../android/OsmAnd/res/values-pl/phrases.xml phrases/pl/
-cp -f ../../android/OsmAnd/res/values-pt/phrases.xml phrases/pt/
-cp -f ../../android/OsmAnd/res/values-pt-rBR/phrases.xml phrases/pt-BR/
+rsync -a ../../android/OsmAnd/res/values-pl/phrases.xml phrases/pl/
+rsync -a ../../android/OsmAnd/res/values-pt/phrases.xml phrases/pt/
+rsync -a ../../android/OsmAnd/res/values-pt-rBR/phrases.xml phrases/pt-BR/
 
-cp -f ../../android/OsmAnd/res/values-ro/phrases.xml phrases/ro-RO/
-cp -f ../../android/OsmAnd/res/values-ru/phrases.xml phrases/ru/
+rsync -a ../../android/OsmAnd/res/values-ro/phrases.xml phrases/ro-RO/
+rsync -a ../../android/OsmAnd/res/values-ru/phrases.xml phrases/ru/
 
-cp -f ../../android/OsmAnd/res/values-sc/phrases.xml phrases/sc/
-cp -f ../../android/OsmAnd/res/values-sk/phrases.xml phrases/sk/
-cp -f ../../android/OsmAnd/res/values-sl/phrases.xml phrases/sl/
-cp -f ../../android/OsmAnd/res/values-sr/phrases.xml phrases/sr/
-cp -f ../../android/OsmAnd/res/values-b+sr+Cyrl/phrases.xml phrases/sr-RS/
-cp -f ../../android/OsmAnd/res/values-b+sr+Latn/phrases.xml phrases/sr-Latn/
-cp -f ../../android/OsmAnd/res/values-sv/phrases.xml phrases/sv/
-cp -f ../../android/OsmAnd/res/values-sq/phrases.xml phrases/sq/
+rsync -a ../../android/OsmAnd/res/values-sc/phrases.xml phrases/sc/
+rsync -a ../../android/OsmAnd/res/values-sk/phrases.xml phrases/sk/
+rsync -a ../../android/OsmAnd/res/values-sl/phrases.xml phrases/sl/
+rsync -a ../../android/OsmAnd/res/values-sr/phrases.xml phrases/sr/
+rsync -a ../../android/OsmAnd/res/values-b+sr+Cyrl/phrases.xml phrases/sr-RS/
+rsync -a ../../android/OsmAnd/res/values-b+sr+Latn/phrases.xml phrases/sr-Latn/
+rsync -a ../../android/OsmAnd/res/values-sv/phrases.xml phrases/sv/
+rsync -a ../../android/OsmAnd/res/values-sq/phrases.xml phrases/sq/
 
-cp -f ../../android/OsmAnd/res/values-tr/phrases.xml phrases/tr/
+rsync -a ../../android/OsmAnd/res/values-tr/phrases.xml phrases/tr/
 
-cp -f ../../android/OsmAnd/res/values-uk/phrases.xml phrases/uk/
-cp -f ../../android/OsmAnd/res/values-uz/phrases.xml phrases/uz-Cyrl/
+rsync -a ../../android/OsmAnd/res/values-uk/phrases.xml phrases/uk/
+rsync -a ../../android/OsmAnd/res/values-uz/phrases.xml phrases/uz-Cyrl/
 
-cp -f ../../android/OsmAnd/res/values-zh-rCN/phrases.xml phrases/zh-Hans/
-## cp -f ../../android/OsmAnd/res/values-zh-rHK/phrases.xml phrases/zh/
-cp -f ../../android/OsmAnd/res/values-zh-rTW/phrases.xml phrases/zh-Hant/
+rsync -a ../../android/OsmAnd/res/values-zh-rCN/phrases.xml phrases/zh-Hans/
+## rsync -a ../../android/OsmAnd/res/values-zh-rHK/phrases.xml phrases/zh/
+rsync -a ../../android/OsmAnd/res/values-zh-rTW/phrases.xml phrases/zh-Hant/
