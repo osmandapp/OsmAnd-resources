7e53da38445223429e241f1f8699739b2670a643
Victor Shcherb <victor.shcherb@gmail.com>
2024-02-14 11:35:44 +0000

    Increase headers to 64 bit length

--- protos/proto_generate.sh
+++ protos/proto_generate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # protobuf/src/protoc --java_out=${DIR}/../../android/OsmAnd-java/src/main/java --proto_path=${DIR} ${DIR}/OBF.proto
-# FIXME other files
- protobuf/src/protoc --java_out=${DIR}/../../android/OsmAnd-java/src/main/java --proto_path=${DIR} ${DIR}/osmand_index.proto
+# protobuf/src/protoc --java_out=${DIR}/../../android/OsmAnd-java/src/main/java --proto_path=${DIR} ${DIR}/osmand_index.proto
 # protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/osmformat.proto
 # protobuf/src/protoc --java_out=${DIR}/../../tools/OsmAndMapCreator/src --proto_path=${DIR} ${DIR}/fileformat.proto
 
