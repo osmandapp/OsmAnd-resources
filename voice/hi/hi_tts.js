c260896230f6f32e5f6d939a07c59b57b6daa89e
Hardy <hardy.mueller@web.de>
2025-01-16 18:30:14 +0100

    Avoid 9/10 of a mile prompt, and use feet from 1000 ft on

--- voice/hi/hi_tts.js
+++ voice/hi/hi_tts.js
@@ -122,7 +122,6 @@ function populateDictionary(tts) {
 	dictionary["kilometers"] = tts ? "किलोमीटर" : "kilometers.ogg";
 
 	dictionary["feet"] = tts ? "पैर" : "feet.ogg";
-	dictionary["1_tenth_of_a_mile"] = tts ? "एक मील का दसवां" : "1_tenth_of_a_mile.ogg";
 	dictionary["tenths_of_a_mile"] = tts ? "एक मील का दसवां" : "tenths_of_a_mile.ogg";
 	dictionary["around_1_mile"] = tts ? "के बारे में 1 मील" : "around_1_mile.ogg";
 	dictionary["miles"] = tts ? "मील" : "miles.ogg";
@@ -172,11 +171,11 @@ function distance(dist) {
 			}
 			break;
 		case "mi-f":
-			if (dist < 160) {
+			if (dist < 91) {
 				return (tts ? (Math.round(2*dist/100.0/0.3048)*50).toString() : ogg_dist(Math.round(2*dist/100.0/0.3048)*50)) + " " + dictionary["feet"];
-			} else if (dist < 241) {
-				return dictionary["1_tenth_of_a_mile"];
-			} else if (dist < 1529) {
+			} else if (dist < 320) {
+				return (tts ? (Math.round(dist/100.0/0.3048)*100).toString() : ogg_dist(Math.round(dist/100.0/0.3048)*100)) + " " + dictionary["feet"];
+			} else if (dist < 1367) {
 				return (tts ? Math.round(dist/161.0).toString() : ogg_dist(Math.round(dist/161.0))) + " " + dictionary["tenths_of_a_mile"];
 			} else if (dist < 2414) {
 				return dictionary["around_1_mile"];
--- voice/sw/sw_tts.js
+++ voice/sw/sw_tts.js
@@ -120,7 +120,6 @@ function populateDictionary(tts) {
 	dictionary["kilometers2"] = tts ? "kilometa" : "kilometers2.ogg";
 
 	dictionary["feet"] = tts ? "futi" : "feet.ogg";
-	dictionary["1_tenth_of_a_mile"] = tts ? "moja ya kumi ya maili" : "1_tenth_of_a_mile.ogg";
 	dictionary["tenths_of_a_mile"] = tts ? "sehemu ya kumi ya maili" : "tenths_of_a_mile.ogg";
 	dictionary["around_1_mile"] = tts ? "kuhusu maili moja" : "around_1_mile.ogg";
 	dictionary["miles"] = tts ? "maili" : "miles.ogg";
@@ -169,11 +168,11 @@ function distance(dist) {
 			}
 			break;
 		case "mi-f":
-			if (dist < 160) {
+			if (dist < 91) {
 				return dictionary["feet"] + " " + (tts ? (Math.round(2*dist/100.0/0.3048)*50).toString() : ogg_dist(Math.round(2*dist/100.0/0.3048)*50));
-			} else if (dist < 241) {
-				return dictionary["1_tenth_of_a_mile"];
-			} else if (dist < 1529) {
+			} else if (dist < 320) {
+				return dictionary["feet"] + " " + (tts ? (Math.round(dist/100.0/0.3048)*100).toString() : ogg_dist(Math.round(dist/100.0/0.3048)*100));
+			} else if (dist < 1367) {
 				return dictionary["tenths_of_a_mile"] + " " + (tts ? Math.round(dist/161.0).toString() : ogg_dist(Math.round(dist/161.0)));
 			} else if (dist < 2414) {
 				return dictionary["around_1_mile"];
